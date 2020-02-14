440f82d31f39b03f7a7e304f4a4d4ba5e179a6b4
Juan Carlos Pinzon <juancarlospinzon@outlook.com>
2020-02-13 23:39:00 -0500

    service for products

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsService } from 'src/app/services/products.service';
 import { Product } from 'src/app/product.model';
 
 @Component({
@@ -8,54 +9,12 @@ import { Product } from 'src/app/product.model';
 })
 export class ProductsComponent implements OnInit {
 
-	products: Product[] = [
-		{
-			id: '1',
-			image: 'assets/images/camiseta.png',
-			title: 'Camiseta',
-			price: 80000,
-			description: 'bla bla bla bla bla'
-		},
-		{
-			id: '2',
-			image: 'assets/images/hoodie.png',
-			title: 'Hoodie',
-			price: 80000,
-			description: 'bla bla bla bla bla'
-		},
-		{
-			id: '3',
-			image: 'assets/images/mug.png',
-			title: 'Mug',
-			price: 80000,
-			description: 'bla bla bla bla bla'
-		},
-		{
-			id: '4',
-			image: 'assets/images/pin.png',
-			title: 'Pin',
-			price: 80000,
-			description: 'bla bla bla bla bla'
-		},
-		{
-			id: '5',
-			image: 'assets/images/stickers1.png',
-			title: 'Stickers',
-			price: 80000,
-			description: 'bla bla bla bla bla'
-		},
-		{
-			id: '6',
-			image: 'assets/images/stickers2.png',
-			title: 'Stickers',
-			price: 80000,
-			description: 'bla bla bla bla bla'
-		},
-	];
+	products: Product[]
 
-	constructor() { }
+	constructor(private productsService: ProductsService) { }
 
 	ngOnInit() {
+		this.products = this.productsService.getAllProducts()
 	}
 
 	clickProduct(id: string) {
