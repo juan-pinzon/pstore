8068100e55858f3af7e98b101f33c4cb75683874
Juan Carlos Pinzon <juancarlospinzon@outlook.com>
2020-02-14 20:48:29 -0500

    Agregar core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { PageNotFoundComponent } from './components/page-not-found/page-not-foun
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 import { LayoutComponent } from './components/layout/layout.component';
 import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
 	declarations: [
@@ -26,7 +27,8 @@ import { SharedModule } from './shared/shared.module';
 		BrowserModule,
 		AppRoutingModule,
 		FormsModule,
-		SharedModule
+		SharedModule,
+		CoreModule
 	],
 	providers: [],
 	bootstrap: [AppComponent]
