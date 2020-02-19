f963d5018471c21c2ffaee714e48c17ec861feb5
Juan Carlos Pinzon <juancarlospinzon@outlook.com>
2020-02-19 17:33:16 -0500

    Remover product del AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,8 @@ import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductComponent } from './components/product.component';
-import { ProductsComponent } from './components/products/products.component';
 import { ContactComponent } from './components/contact/contact.component'
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
-import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 import { LayoutComponent } from './components/layout/layout.component';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
@@ -16,11 +13,8 @@ import { CoreModule } from './core/core.module';
 @NgModule({
 	declarations: [
 		AppComponent,
-		ProductComponent,
-		ProductsComponent,
 		ContactComponent,
 		PageNotFoundComponent,
-		ProductDetailComponent,
 		LayoutComponent
 	],
 	imports: [
