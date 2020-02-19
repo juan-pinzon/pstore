89977e002b2dcba50c7e97bf9ab77c0ba548a1c1
Juan Carlos Pinzon <juancarlospinzon@outlook.com>
2020-02-19 17:33:44 -0500

    Colocar modulo de rutas para que funcionene en el header

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { ExponentialPipe } from './pipes/exponential/exponential.pipe';
 import { HighlightDirective } from './directives/highlight/highlight.directive';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-
 
 
 @NgModule({
@@ -21,7 +22,8 @@ import { FooterComponent } from './components/footer/footer.component';
 		FooterComponent
 	],
 	imports: [
-		CommonModule
+		CommonModule,
+		RouterModule
 	]
 })
 export class SharedModule { }
