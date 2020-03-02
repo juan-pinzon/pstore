f26b78b8e6a76b8efb27f7c457bc1973d3a0f65c
Juan Carlos Pinzon <juancarlospinzon@outlook.com>
2020-03-02 16:34:43 -0500

    Añadir material al sharedModule

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,15 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatButtonModule} from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatBadgeModule } from '@angular/material/badge';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
 	declarations: [],
 	imports: [
 		CommonModule,
-		MatButtonModule
+		MatIconModule,
+		MatButtonModule,
+		MatBadgeModule,
+		MatToolbarModule,
+		MatCardModule
 	],
 	exports: [
-		MatButtonModule
+		MatIconModule,
+		MatButtonModule,
+		MatBadgeModule,
+		MatToolbarModule,
+		MatCardModule
 	]
 })
 export class MaterialModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,6 +6,7 @@ import { ExponentialPipe } from './pipes/exponential/exponential.pipe';
 import { HighlightDirective } from './directives/highlight/highlight.directive';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { MaterialModule } from '../material/material.module';
 
 
 @NgModule({
@@ -23,7 +24,8 @@ import { FooterComponent } from './components/footer/footer.component';
 	],
 	imports: [
 		CommonModule,
-		RouterModule
+		RouterModule,
+		MaterialModule
 	]
 })
 export class SharedModule { }
