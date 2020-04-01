e2502e234e2c0df64cd22c59d2328c86d6557e57
Juan Carlos Pinzon <juancarlospinzon@outlook.com>
2020-04-01 18:19:38 -0500

    Remove log

--- src/app/utils/Validators.ts
+++ src/app/utils/Validators.ts
@@ -2,7 +2,6 @@ import { AbstractControl } from '@angular/forms'
 
 export const isPriceValidator = (control: AbstractControl) => {
 	const value = control.value
-	console.log(value)
 	if (value > 10000) {
 		return {price_invalid: true}
 	}
