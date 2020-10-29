bfce4c28f89ec6755c41e9d69e3a658fc577138b
Juan Carlos Pinzon <juancarlospinzon@outlook.com>
2020-10-29 13:45:36 -0500

    isNumber Validator

--- src/app/utils/Validators.ts
+++ src/app/utils/Validators.ts
@@ -7,3 +7,16 @@ export const isPriceValidator = (control: AbstractControl) => {
 	}
 	return null
 }
+
+function isNumber(value: string) {
+	return !isNaN(parseInt(value, 10))
+}
+
+export const passwordValidator = (control: AbstractControl) => {
+	const value = control.value
+	const isNum = value.split('').find(isNumber)
+	if (isNum !== undefined) {
+		return {invalid_password: true}
+	}
+	return null
+}
